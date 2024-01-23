90dc6423b8f1f6833e74534e6ac9883b174ea1c9
RAKSHANASRIK <rakshanasriece@gmail.com>
2024-01-23 22:03:16 +0530

    i dhoni

---
+++ es6.js
@@ -0,0 +1,22 @@
+//ES6 -> ECMA Script 6 Standard
+//1.Var,let,const => refer variable.js
+//2. Arrow function
+/*var fun=() =>{
+console.log("Hello")
+}
+fun()
+
+//3.Destructuring operator
+var array =[10,20,30,40,50]
+var [a,b,c,d,e]=array
+console.log(a,b,c,d,e)
+
+//4.Spread operator
+var array =[10,20,30,40,50]
+var arr1=[...array,60,70]
+console.log(arr1)*/
+
+
+
+
+
